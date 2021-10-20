649edccb184f7a1f0c2926b011dd584094db8341
Abi Radzhabova <aradzhabova@askartec.com>
2021-10-20 11:49:00 +0600

    Initial commit

---
+++ swift-feed/swift-feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-feed
+//
+//  Created by Abi  Radzhabova on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
